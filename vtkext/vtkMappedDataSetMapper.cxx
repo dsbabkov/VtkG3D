6408cb39598e6ef8dc671a90c67cbd37235e28cf
Dmitriy Babkov <dsbabkov@gmail.com>
2020-06-18 09:05:32 +0300

    Доработка до рабочего состояния

--- vtkext/vtkMappedDataSetMapper.cxx
+++ vtkext/vtkMappedDataSetMapper.cxx
@@ -119,8 +119,8 @@ void vtkMappedDataSetMapper::Render(vtkRenderer *ren, vtkActor *act)
   this->PolyDataMapper->SetUseLookupTableScalarRange(
     this->GetUseLookupTableScalarRange());
   this->PolyDataMapper->SetScalarRange(this->GetScalarRange());
-  this->PolyDataMapper->SetImmediateModeRendering(
-    this->GetImmediateModeRendering());
+//  this->PolyDataMapper->SetImmediateModeRendering(
+//    this->GetImmediateModeRendering());
   this->PolyDataMapper->SetColorMode(this->GetColorMode());
   this->PolyDataMapper->SetInterpolateScalarsBeforeMapping(
                                this->GetInterpolateScalarsBeforeMapping());
